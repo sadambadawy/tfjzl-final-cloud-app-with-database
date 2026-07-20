0ff8f2d0876f1063d7975cb5018dcbcaa532b691
sadambadawy <sadambadawy@gmail.com>
2026-07-20 14:03:21 -0400

    t5

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path('<int:course_id>/submit/', views.submit, name="submit"),
     path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
 
     # <HINT> Create a route for submit view
 
